6095ad83ba8b0aadaa8da97d29d735d6e0047ecd
Arthur Chanda <arthchad@yahoo.com>
2019-06-05 22:22:41 +0200

    Update mean.r
    2nd time

--- mean.r
+++ mean.r
@@ -1,5 +1,5 @@
 # My starts prog
-x <- c(12, 7,3, 4.2,18, 2, 54,-21, 8, -5)
+x <- c(12, 7,3, 4.2,18, 2, 54,-21, 8, -5, NA)
 #Find mean
-result.mean <- mean(x, trim = 0.3)
+result.mean <- mean(x, na.rm = TRUE)
 print(result.mean)
